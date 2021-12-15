659320a47fe5c55d6777ba6624c718e6c45d5125
Usman Akram <usman.akram@gmail.com>
2021-12-15 23:46:09 +0500

    Update apiauth.js

--- middlewares/apiauth.js
+++ middlewares/apiauth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const User = require("../models/user");
+const User = require("../models/User");
 async function apiauth(req, res, next) {
   const token = req.header("x-auth-token");
 
