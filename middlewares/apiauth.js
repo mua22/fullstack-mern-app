9765151898b70d5d3295b1f3d8c04629ade06ca3
Usman Akram <usman.akram@gmail.com>
2021-12-22 17:23:06 +0500

    auth fixed

--- middlewares/apiauth.js
+++ middlewares/apiauth.js
@@ -8,8 +8,8 @@ async function apiauth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-    req.user = decoded;
-    const user = await User.findById(req.user._id).select("-password");
+
+    const user = await User.findById(decoded._id).select("-password");
     if (!user) return res.status(400).send("Invalid token: User Dont exist");
     req.user = user;
     next();
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,17 +4,21 @@ var User = require("../../models/User");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
 const config = require("config");
-
+const bcrypt = require("bcryptjs");
 router.post("/", async function (req, res, next) {
   let user = await User.findOne({
     email: req.body.email,
   });
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).send("Invalid Password");
+  }
   const token = jwt.sign(
     {
       _id: user._id,
-      role: user.role,
+      roles: user.roles,
       name: user.name,
-      phone: user.phone,
+
       email: user.email,
     },
     config.get("jwtPrivateKey")
