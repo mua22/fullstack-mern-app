a0ea5e6e28791b176472d8fc3f5aa38434da1a07
Usman Akram <usman.akram@gmail.com>
2024-01-23 12:28:31 +0500

    done

--- routes/index.js
+++ routes/index.js
@@ -47,12 +47,5 @@ router.post("/register", async function (req, res, next) {
 router.get("/contact-us", function (req, res, next) {
   return res.render("site/contact", { layout: "layout" });
 });
-router.get("/", async function (req, res, next) {
-  let products = await Product.find();
-  return res.render("site/homepage", {
-    pagetitle: "Awesome Products",
-    products,
-  });
-});
 
 module.exports = router;
